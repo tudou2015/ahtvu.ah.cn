4aa3b7d15aad4e663a386a5ba4262f78fdd0e4ef
zmbanne <994120936@qq.com>
2016-08-29 20:48:16 +0800

    动态获取通知公告、分校之窗
    动态获取通知公告、分校之窗

---
+++ themes/default/widgets/portal_school_news/data.js
@@ -0,0 +1,46 @@
+const util = require('util');
+
+module.exports = function (req, res, utils) {
+
+    var deferred = Promise.defer();
+
+    utils.request({
+        url: 'open/get_posts_by_category',
+        method: 'POST',
+        qs: {
+            siteId: req.app.site.id,
+            categoryId: 'otppag6m44tniccoabr87q',
+            pageSize: 16
+        }
+    }, function (result) {
+
+        var data = {
+            category: {},
+            list: []
+        };
+
+        if (result.code != 200) {
+
+            deferred.resolve(data);
+            return;
+        };
+
+        result.body = JSON.parse(result.body);
+
+        data.category = { href: util.format('/portal/category?id=%s', result.body.category.id) };
+        result.body.data.forEach(function (e) {
+
+            data.list.push({
+                ori_title: e.title,
+                title: utils.subString(e.title, 30),
+                date: e.date_published,
+                href: utils.urlFormat(util.format('/detail?id=%s', e.id))
+            });
+
+        }, this);
+
+        deferred.resolve({ data: data });
+    });
+
+    return deferred.promise;
+}
