8726dcf2f862d9ccfae2d3e331ef86f26b6bfc66
Lei Xu <leixu@txtek.cn>
2016-12-21 14:50:08 +0800

    新建信息与工程学院子站

---
+++ themes/xgxy/widgets/portal_txtnews/data.js
@@ -0,0 +1,67 @@
+const util = require('util');
+
+module.exports = function (req, res, utils) {
+
+    var deferred = Promise.defer();
+
+    utils.request({
+        url: 'open/get_posts_by_category',
+        method: 'POST',
+        qs: {
+            siteId: req.app.site.id,
+            categoryId: 'xfdwaogmw6noxp-pegfjyq',
+            pageSize: 5
+        }
+    }, function (result) {
+
+        var data = {
+            category: {},
+            list: [],
+            first: undefined
+        };
+
+        if (result.code != 200) {
+
+            deferred.resolve(data);
+            return;
+        };
+
+        result.body = JSON.parse(result.body);
+        data.category = {
+            href: util.format('category?id=%s', result.body.category.id)
+        };
+
+        result.body.data.forEach(function (e) {
+
+            //设置第一个显示的新闻                    
+            if (!data.first) {
+
+                e.text ? e.text : (e.text = e.title);
+                e.image_url ? (e.image_url = util.format('%s&width=171&height=105', e.image_url)) : '';
+
+                data.first = {
+                    image: e.image_url,
+                    title: utils.subString(e.title, 13),
+                    text: (e.image_url ? utils.subString(e.text, 27) : utils.subString(e.text, 50)),
+                    href: util.format('detail?id=%s', e.id)
+                };
+
+                return false;
+            }
+
+            data.list.push({
+                ori_title: e.title,
+                title: utils.subString(e.title, 25),
+                date: e.date_published,
+                href: util.format('detail?id=%s', e.id)
+            });
+
+        }, this);
+
+        deferred.resolve({
+            data: data
+        });
+    });
+
+    return deferred.promise;
+}
