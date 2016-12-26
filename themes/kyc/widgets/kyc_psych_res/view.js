e20ffe5bef33c3f7b07f74da07d3115c94682ff1
zmbanne <994120936@qq.com>
2016-12-26 17:06:18 +0800

    修复科研处三级页面中分页冲突问题

--- themes/kyc/widgets/kyc_psych_res/view.js
+++ themes/kyc/widgets/kyc_psych_res/view.js
@@ -1,9 +1,9 @@
 $(function () {
 
     //绑定分页信息    
-    var options = $('.series_paging').data('paging');
+    var options = $('.res_paging').data('paging');
 
     options.show_left_info = false;
 
-    $('.series_paging').paging(options);
+    $('.res_paging').paging(options);
 });
