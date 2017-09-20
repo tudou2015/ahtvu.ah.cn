fab5b154307eb5e2f9db472eb9005e364e25400c
ahwyj <50221891@qq.com>
2017-09-20 09:49:00 +0800

    继续教育园区

---
+++ themes/jxjyyq/widgets/jxjyyq_link/data.js
@@ -0,0 +1,51 @@
+const util = require('util');
+
+module.exports = function (req, res, utils) {
+
+    var deferred = Promise.defer();
+
+    utils.request({
+        url: 'open/get_posts_by_category',
+        method: 'POST',
+        qs: {
+            siteId: req.app.site.id,
+            categoryId: 'eiysan-nsk9ectnpfppnzw',
+            withChildren: true,
+            sort: 'sortOrder',
+            pageSize: 6
+        }
+    }, function (result) {
+
+        var data = {
+            category: {},
+            paging: {},
+            list: []
+        };
+
+        if (result.code != 200) {
+
+            deferred.resolve(data);
+            return;
+        };
+
+        result.body = JSON.parse(result.body);
+
+        result.body.data.forEach(function (e) {
+
+            var props = JSON.parse(e.props);
+
+            data.list.push({
+                title: e.title,
+                date: e.date_published,
+                href: props.href || 'javascript:void(0);',
+            });
+
+        }, this);
+
+        deferred.resolve({
+            data: data
+        });
+    });
+
+    return deferred.promise;
+}
